1c87516f273d8c1afe0f902831f6a111f4fb009f
Fnvss <fnvss@iscte-iul.pt>
2022-12-13 14:54:21 +0000

    Availabilty by color in MonthlyCal

--- src/main/java/ES_LETI_1Semestre_2022_GRUPO_07/ES_LETI_1Semestre_2022_GRUPO_07/Main.java
+++ src/main/java/ES_LETI_1Semestre_2022_GRUPO_07/ES_LETI_1Semestre_2022_GRUPO_07/Main.java
@@ -1,12 +1,8 @@
 package ES_LETI_1Semestre_2022_GRUPO_07.ES_LETI_1Semestre_2022_GRUPO_07;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import net.fortuna.ical4j.data.ParserException;
 
@@ -14,42 +10,9 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, ParserException, ParseException {
 		// TODO Auto-generated method stub
+		PrintStream out = new PrintStream("Horário.txt");
+        System.setOut(out);
 		Login teste = new Login();
-		
-//		List<Event> events = new ArrayList<>();
-//		List<Element> elements = new ArrayList<>();
-//		Schedule schedule = new Schedule(events, elements);
-//		
-//		Element element1 = new Element("Guilherme", "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=ggmso@iscte.pt&password=RapiAbBUH3jevyDETvtVAO2iI7JUi9NTv0i7JAn8tiUpVOAES55J6exaS9U27XMosaQINnY17SXwSe561qHbRbEtUqWTn7SfbiiuQi92WogIhg6U7JJpAhqjvjo3HdOV");
-//		Element element2 = new Element("Tomás", "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=tlpco@iscte.pt&password=peZuWATHi62pUmdMkFVyVLgeqWYc1Wk9VTCE3hbcbek93N030ZkPxDuiJPHmJP169nua5GMkztSGKD9MXczPYZCXqnUmrBvwklOcRftGPql225ahEjieA2782mKLpEQA");
-//		Element element3= new Element("Rafael", "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=ramss3@iscte.pt&password=gGdsqZehHOOyFFAPMPSk8VeB63LPV0scP4qR36srE3dMgEB3b8GPIxCKorPUzQeySmm0XoAR7K0gw8pA5QrVrvx1VwfVrgjOFhMlC0mQkG6UxWuE4R2n12xxgV9hYfbX");
-//		Element element4= new Element("Filipe", "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=fnvss@iscte.pt&password=QXPQEvIYJf6oR7oX3P7Ua19IGGIJTUD2Zmd6q7aqOaxmefDktt4TD0rSiMefsxnRwD8C9w4FmwWsKqF8wIPpxrTVisf31hZMd8KUAUERCtDWOfeWnM64j7k1jV8jbVE6");
-//		schedule.addElementToSchedule(element1);
-//		schedule.addElementToSchedule(element2);
-//		schedule.addElementToSchedule(element3);
-//		schedule.addElementToSchedule(element4);
-//		List<Event> list = schedule.getEvents();
-//		List<Element> listTest = new ArrayList<>();
-//		listTest.add(element4);
-//		listTest.add(element3);
-//		schedule.checkAvailableDate(listTest, "manhã", 65);
-//		//List<Event> list = schedule.readCalendar(element3).getEvents();
-		//List<Event> list = schedule.readAllCalendars().getEvents();
-		//List<Event> list = schedule.readCalendarForSpecificMonth("may").getEvents();
-		//List<Event> list = schedule.readCalendarForSpecificDayOfTheMonth(3, "january").getEvents();
-//				for(Event e : list) {
-//					System.out.println(e.toString());
-//				}
-		//schedule.checkHours("2023", 58);
-		//		List<Element> newElements = new ArrayList<>();
-		//		//newElements.add(element4);
-		//		newElements.add(element3);
-		//		List<Event> ver = schedule.checkAvailableDate(LocalDateTime.of(2022, 11, 28, 0, 0), newElements, "tarde", null);
-		//		System.out.println(schedule.getElements().stream().anyMatch(newElements::contains));
-
-
-		//		}
-
 
 	}
 
--- src/main/java/ES_LETI_1Semestre_2022_GRUPO_07/ES_LETI_1Semestre_2022_GRUPO_07/MonthlyCalendar.java
+++ src/main/java/ES_LETI_1Semestre_2022_GRUPO_07/ES_LETI_1Semestre_2022_GRUPO_07/MonthlyCalendar.java
@@ -3,17 +3,25 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
 
+import ES_LETI_1Semestre_2022_GRUPO_07.ES_LETI_1Semestre_2022_GRUPO_07.Event;
 import net.fortuna.ical4j.data.ParserException;
 
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MonthlyCalendar{
+
 	static JLabel lblMonth;
-	static JButton btnPrev, btnNext;
+	static JButton btnPrev, btnNext, backBtn;
 	static JTable tblCalendar;
 	static JComboBox cmbYear, cmbView;
 	static JFrame frmMain;
@@ -22,6 +30,10 @@ public class MonthlyCalendar{
 	static JScrollPane stblCalendar; //The scrollpane
 	static JPanel pnlCalendar;
 	static int realYear, realMonth, realDay, currentYear, currentMonth;
+	static LocalDate actualDate;
+
+
+
 
 	public static void main (String args[]){
 		//Look and feel
@@ -33,18 +45,19 @@ public class MonthlyCalendar{
 
 		//Prepare frame
 		frmMain = new JFrame ("Calendar"); //Create frame
-		frmMain.setSize(1000, 900); //Set size to 400x400 pixels
+		frmMain.setSize(1600, 1000); //Set size to 400x400 pixels
 		pane = frmMain.getContentPane(); //Get content pane
 		pane.setLayout(null); //Apply null layout
 		frmMain.setLocationRelativeTo(null);
 		frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Close when X is clicked
+
 
 		//Create controls
 		String[] typeOfView = {"Monthly","Daily", "Weekly"};
 		cmbView = new JComboBox(typeOfView);
 		cmbView.setToolTipText("View");
 		lblMonth = new JLabel ("January");
-		lblMonth.setFont(new Font("Calibri", Font.BOLD, 18));
+		lblMonth.setFont(new Font("Calibri", Font.BOLD, 21));
 		cmbYear = new JComboBox();
 		btnPrev = new JButton ("<");
 		btnNext = new JButton (">");
@@ -60,6 +73,61 @@ public class MonthlyCalendar{
 		cmbYear.addActionListener(new cmbYear_Action());
 		cmbView.addActionListener(new cmbView_Action());
 
+		backBtn = new JButton("Back");
+		backBtn.addActionListener(e -> {
+			switch (cmbView.getSelectedItem().toString()) {
+			case "Monthly":
+				Login.getInstance().getFrameInstance().setVisible(true);
+				frmMain.dispose();
+				break;
+			}
+		});
+
+		// Create a MouseListener to handle the mouse click event on the calendar table
+		MouseListener mouseListener = new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 1) {
+					// Get the row and column indices of the clicked cell
+					int row = tblCalendar.rowAtPoint(e.getPoint());
+					int col = tblCalendar.columnAtPoint(e.getPoint());
+
+					// Get the value of the clicked cell
+					int day = (Integer) tblCalendar.getValueAt(row, col);
+
+					actualDate = LocalDate.of(currentYear,currentMonth + 1,day);
+
+
+					// Check if the cell contains a day of the month
+					if (day != 0) {
+						try {
+							DayCalendar aux = new DayCalendar(CalendarViews.getListOfEvents(), actualDate);
+							CalendarViews.dailyView(aux);
+						} catch (IOException | ParserException | ParseException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} 
+						frmMain.dispose();
+					}
+				}
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+
+			@Override
+			public void mouseExited(MouseEvent e) {}
+		};
+
+		// Add the MouseListener to the calendar table
+		tblCalendar.addMouseListener(mouseListener);
+
 		//Add controls to pane
 		pane.add(pnlCalendar);
 		pnlCalendar.add(cmbView);
@@ -67,16 +135,18 @@ public class MonthlyCalendar{
 		pnlCalendar.add(cmbYear);
 		pnlCalendar.add(btnPrev);
 		pnlCalendar.add(btnNext);
+		pnlCalendar.add(backBtn);
 		pnlCalendar.add(stblCalendar);
 
 		//Set bounds
-		pnlCalendar.setBounds(0, 0, 1000, 900);
-		lblMonth.setBounds(500-lblMonth.getPreferredSize().width/2, 7, 100, 25);
-		cmbYear.setBounds(890, 826, 80, 25);
-		cmbView.setBounds(890, 7, 80, 27);
-		btnPrev.setBounds(350, 7, 50, 25);
-		btnNext.setBounds(600, 7, 50, 25);
-		stblCalendar.setBounds(0, 40, 1000, 900);
+		pnlCalendar.setBounds(0, 0, 1600, 1000);
+		lblMonth.setBounds(800-lblMonth.getPreferredSize().width/2, 9, 100, 25);
+		cmbYear.setBounds(1490, 920, 80, 25);
+		cmbView.setBounds(570, 7, 80, 25);
+		btnPrev.setBounds(670, 7, 50, 25);
+		btnNext.setBounds(880, 7, 50, 25);
+		backBtn.setBounds(950, 7, 70, 25);
+		stblCalendar.setBounds(0, 40, 1600, 1000);
 
 		//Make frame visible
 		frmMain.setVisible(true);
@@ -108,7 +178,7 @@ public class MonthlyCalendar{
 		tblCalendar.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
 		//Set row/column count
-		tblCalendar.setRowHeight(125);
+		tblCalendar.setRowHeight(140);
 		mtblCalendar.setColumnCount(7);
 		mtblCalendar.setRowCount(6);
 
@@ -132,7 +202,7 @@ public class MonthlyCalendar{
 		if (month == 0 && year <= realYear-10){btnPrev.setEnabled(false);} //Too early
 		if (month == 11 && year >= realYear+100){btnNext.setEnabled(false);} //Too late
 		lblMonth.setText(months[month]); //Refresh the month label (at the top)
-		lblMonth.setBounds(500-lblMonth.getPreferredSize().width/2, 7, 100, 25); //Re-align label with calendar
+		lblMonth.setBounds(800-lblMonth.getPreferredSize().width/2, 10, 100, 25); //Re-align label with calendar
 		cmbYear.setSelectedItem(String.valueOf(year)); //Select the correct year in the combo box
 
 		//Clear table
@@ -158,18 +228,47 @@ public class MonthlyCalendar{
 		tblCalendar.setDefaultRenderer(tblCalendar.getColumnClass(0), new tblCalendarRenderer());
 	}
 
+
 	static class tblCalendarRenderer extends DefaultTableCellRenderer{
 		public Component getTableCellRendererComponent (JTable table, Object value, boolean selected, boolean focused, int row, int column){
+			Schedule schedule = Login.schedule;
+			List<Event> list = schedule.getEvents();
+
 			super.getTableCellRendererComponent(table, value, selected, focused, row, column);
 			if (column == 0 || column == 6){ //Week-end
-				setBackground(new Color(255, 220, 220));
+				setBackground(new Color(211, 211, 211, 128));
 			}
 			else{ //Week
 				setBackground(new Color(255, 255, 255));
 			}
+
+
 			if (value != null){
+				// Convert the cell value to a LocalDate object
+				int day = Integer.parseInt(value.toString());
+				// Convert the cell value to a LocalDate object
+				LocalDate date = LocalDate.of(currentYear, currentMonth+1, day);
+
+				// Filter the list of events to only include events that happen on the same date as the cell date
+				List<Event> filteredList = list.stream()
+						.filter(event -> event.getStartDate().toLocalDate().equals(date))
+						.collect(Collectors.toList());
+
+				if(filteredList.size() == 0) {
+				} else if(filteredList.size() <= 1) {
+					setBackground(new Color(153,255,51));//GREEN
+				} else if(filteredList.size() <= 2) {
+					setBackground(new Color(255,255,102));//YELLOW
+				} else if(filteredList.size() <= 3) {
+					setBackground(Color.ORANGE);//Orange
+				} else if(filteredList.size() >= 4) {
+					setBackground(new Color(204,0,0));//Red
+				}
+
+
 				if (Integer.parseInt(value.toString()) == realDay && currentMonth == realMonth && currentYear == realYear){ //Today
 					setBackground(new Color(220, 220, 255));
+
 				}
 			}
 			setBorder(null);
@@ -177,6 +276,7 @@ public class MonthlyCalendar{
 			return this;
 		}
 	}
+
 
 	static class btnPrev_Action implements ActionListener{
 		public void actionPerformed (ActionEvent e){
@@ -190,6 +290,7 @@ public class MonthlyCalendar{
 			refreshCalendar(currentMonth, currentYear);
 		}
 	}
+
 	static class btnNext_Action implements ActionListener{
 		public void actionPerformed (ActionEvent e){
 			if (currentMonth == 11){ //Foward one year
@@ -202,6 +303,7 @@ public class MonthlyCalendar{
 			refreshCalendar(currentMonth, currentYear);
 		}
 	}
+
 	static class cmbYear_Action implements ActionListener{
 		public void actionPerformed (ActionEvent e){
 			if (cmbYear.getSelectedItem() != null){
@@ -211,27 +313,31 @@ public class MonthlyCalendar{
 			}
 		}
 	}
+
 	static class cmbView_Action implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			if(cmbView.getSelectedItem() == "Daily") {
+			if(cmbView.getSelectedItem().toString()=="Weekly") {
 				try {
-					CalendarViews.dailyView();
+					WeekCalendar aux = new WeekCalendar(CalendarViews.getListOfEvents());
+					CalendarViews.weeklyView(aux);
 				} catch (IOException | ParserException | ParseException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				frmMain.setVisible(false);
+				frmMain.dispose();
 			}
-			
-			if(cmbView.getSelectedItem() == "Weekly") {
+
+			if(cmbView.getSelectedItem().toString()=="Daily") {		
 				try {
-					CalendarViews.weeklyView();
+					DayCalendar aux = new DayCalendar(CalendarViews.getListOfEvents(), LocalDate.now());
+					CalendarViews.dailyView(aux);
 				} catch (IOException | ParserException | ParseException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}
-				frmMain.setVisible(false);
+				}			
+				frmMain.dispose();
 			}
 		}
 	}
+
 }
