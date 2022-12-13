4588ccf2e5ea341f94011b302b8d840146aa06ee
ggmso <ggmso@iscte-iul.pt>
2022-12-13 20:16:43 +0000

    Main Update

--- src/main/java/ES_LETI_1Semestre_2022_GRUPO_07/ES_LETI_1Semestre_2022_GRUPO_07/Main.java
+++ src/main/java/ES_LETI_1Semestre_2022_GRUPO_07/ES_LETI_1Semestre_2022_GRUPO_07/Main.java
@@ -3,6 +3,11 @@ package ES_LETI_1Semestre_2022_GRUPO_07.ES_LETI_1Semestre_2022_GRUPO_07;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import net.fortuna.ical4j.data.ParserException;
 
@@ -10,10 +15,9 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, ParserException, ParseException {
 		// TODO Auto-generated method stub
-		PrintStream out = new PrintStream("Horário.txt");
-        System.setOut(out);
+//		PrintStream out = new PrintStream("Horário.txt");
+//		System.setOut(out);
 		Login teste = new Login();
-
 	}
 
 
